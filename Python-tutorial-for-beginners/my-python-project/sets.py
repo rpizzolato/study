1e1add4e5ce743824f1fa63b8fb3ffc3d4f3fddd
Rodrigo <rpessolato@gmail.com>
2023-01-11 16:07:46 -0300

    stopped at 4:37:48

---
+++ Python-tutorial-for-beginners/my-python-project/sets.py
@@ -0,0 +1,18 @@
+# remember to use curly braces {} instead of square bracket []
+my_set = {"January", "February", "March"}
+
+# to print a set, use for in
+for element in my_set:
+    print(element)
+
+# to add an element in the set
+my_set.add("April")
+print(my_set)
+
+# to remove an element in the set or list
+my_set.remove("January")
+print(my_set)
+
+my_list = ["January", "February", "March"]
+my_list.remove("January")
+print(my_list)
