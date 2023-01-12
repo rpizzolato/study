6336512223d4d633c46f63899c94fff6909f78e6
Rodrigo <rpessolato@gmail.com>
2023-01-11 22:40:15 -0300

    stopped at 4:44:03

--- Python-tutorial-for-beginners/automation-with-python/main.py
+++ Python-tutorial-for-beginners/automation-with-python/main.py
@@ -12,6 +12,7 @@ for product_row in range(2, product_list.max_row + 1):
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
     product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
 
     # calculation number of products per supplier
     if supplier_name in products_per_supplier:
@@ -34,6 +35,11 @@ for product_row in range(2, product_list.max_row + 1):
     if inventory < 10:
         products_under_10_inv[int(product_num)] = int(inventory)
 
+    # add value for total inventory price
+    inventory_price.value = inventory * price
+
 print(products_per_supplier)
 print(total_value_per_supplier)
 print(products_under_10_inv)
+
+inv_file.save("inventory_with_total_value.xlsx")
